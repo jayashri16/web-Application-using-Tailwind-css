cc0fc13ec09e4262c35e974e1889ccf3a4b1c496
JAYASHRI <141231703+jayashri16@users.noreply.github.com>
2024-04-16 20:13:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import AuthInputs from '../src/components/AuthInput.jsx';
+import Header from '../src/components/Header.jsx';
+import Button from './components/Button.jsx';
+import Input from './components/Input.jsx';
+import './/index.css';
+export default function App() {
+  return (
+    <>
+   
+      <Header />
+
+      <main>
+   
+        <AuthInputs />  
+       
+      </main>
+      
+    </>
+  );
+}
